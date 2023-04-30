ab07af42f20a90713d94b3c0382f8beabeebb105
ianacodev <ianaco.dev@gmail.com>
2023-04-29 20:09:50 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
-
+// models
+interface Item {
+  name: string;
+  image: string;
+}
+// consts
+const imageAssetUrl = 'assets/images';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'deployment-demo';
+  title = 'Deployment Demo';
+  items: Item[] = [
+    {
+      name: 'angular',
+      image: `${imageAssetUrl}/angular.webp`,
+    },
+    {
+      name: 'docker',
+      image: `${imageAssetUrl}/docker.webp`,
+    },
+    {
+      name: 'kubernetes',
+      image: `${imageAssetUrl}/kubernetes.webp`,
+    },
+    {
+      name: 'nginx',
+      image: `${imageAssetUrl}/nginx.webp`,
+    },
+  ];
 }
